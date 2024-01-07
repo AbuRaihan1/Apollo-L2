df59fcb9f2992d2f6ad78b866d075a8d51913610
Abu Raihan <raihanahmed01973@gmail.com>
2024-01-07 15:17:11 +0600

    but if we used babel then we can use jsx

--- Front-end-track/React-fundamental/explore-manual-react/main.js
+++ Front-end-track/React-fundamental/explore-manual-react/main.js
@@ -1,8 +1,16 @@
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const App = React.createElement(
-  "h1",
-  { style: { color: "red" } },
-  "Amar sonar bangla"
-);
+// const App = React.createElement(
+//   "h1",
+//   { style: { color: "red" } },
+//   "Amar sonar bangla"
+// );
 
+const App = () => {
+  return (
+    <div>
+      <h2>Amar sonar bangla</h2>
+    </div>
+  );
+};
 
+root.render(<App />);
