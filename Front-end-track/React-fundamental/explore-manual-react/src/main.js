30da9807b9c7d6bfdba815bd5f9eb6ce985572eb
Abu Raihan <raihanahmed01973@gmail.com>
2024-01-09 19:55:48 +0600

    updated readME file

--- Front-end-track/React-fundamental/explore-manual-react/src/main.js
+++ Front-end-track/React-fundamental/explore-manual-react/src/main.js
@@ -1,10 +1,10 @@
 import { CheckingForBundle } from "./another";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// const App = React.createElement(
-//   "h1",
-//   { style: { color: "red" } },
-//   "Amar sonar bangla"
-// );
+const Apps = React.createElement(
+  "h1",
+  { style: { color: "red" } },
+  "Testing apps project"
+);
 
 const App = () => {
   {
